636bc3bfd3cc34796089b37d9fc1f671c0c6cbce
nastya.panysh@gmail.com <nastya.panysh@gmail.com>
2023-08-27 23:58:10 +0300

    add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,12 +8,14 @@ function Header() {
       <Text size="lg" weight={500}>
         WHAT SHOULD U WATCH
       </Text>
-      <Link to="/login">
-        <Button>Sign in</Button>
-      </Link>
-      <Link to="/reg">
-        <Button variant="outline">Sign up</Button>
-      </Link>
+      <div className={style.containerBtns}>
+        <Link to="/login">
+          <Button>Sign in</Button>
+        </Link>
+        <Link to="/reg">
+          <Button variant="outline">Sign up</Button>
+        </Link>
+      </div>
     </div>
   );
 }
