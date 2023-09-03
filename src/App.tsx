b093d297307d6ba944b9feed7a069b37afc98a92
nastya.panysh@gmail.com <nastya.panysh@gmail.com>
2023-09-03 17:36:01 +0300

    in process

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegistrationPage from './pages/RegistrationPage';
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<HomePage/>}></Route>
-      <Route path='/login' element={<LoginPage/>}></Route>
-      <Route path='/reg' element={<RegistrationPage/>}></Route>
-
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/reg" element={<RegistrationPage />}></Route>
     </Routes>
   );
 }
--- src/pages/RegistrationPage/index.tsx
+++ src/pages/RegistrationPage/index.tsx
@@ -1,4 +1,3 @@
-import { useToggle, upperFirst } from '@mantine/hooks';
 import {
   TextInput,
   PasswordInput,
@@ -11,31 +10,38 @@ import {
   Checkbox,
   Anchor,
   Stack,
-} from '@mantine/core';
-import { GoogleButton } from '../../lips/SocialButtons';
+} from "@mantine/core";
+import { GoogleButton } from "../../lips/SocialButtons";
 
-import { useForm } from '@mantine/form';
+import { useForm } from "@mantine/form";
+import { Link } from "react-router-dom";
 
 function RegistrationPage(props: PaperProps) {
-  const [type, toggle] = useToggle(['register','login']);
   const form = useForm({
     initialValues: {
-      email: '',
-      name: '',
-      password: '',
+      email: "",
+      name: "",
+      password: "",
       terms: true,
     },
 
     validate: {
-      email: (val: string) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
-      password: (val: string) => (val.length < 6 ? 'Password should include at least 6 characters' : null),
+      email: (val: string) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
+      password: (val: string) =>
+        val.length < 6 ? "Password should include at least 6 characters" : null,
     },
   });
 
   return (
-    <Paper style={{ width: '600px',margin: '0 auto' }} radius="md" p="xl" withBorder {...props}>
+    <Paper
+      style={{ width: "600px", margin: "0 auto" }}
+      radius="md"
+      p="xl"
+      withBorder
+      {...props}
+    >
       <Text size="lg" weight={500}>
-        Welcome to WHAT SHOULD U WATCH, {type} with
+        Welcome to WHAT SHOULD U WATCH, register with
       </Text>
 
       <Group grow mb="md" mt="md">
@@ -46,22 +52,24 @@ function RegistrationPage(props: PaperProps) {
 
       <form onSubmit={form.onSubmit(() => {})}>
         <Stack>
-          {type === 'register' && (
-            <TextInput
-              label="Name"
-              placeholder="Your name"
-              value={form.values.name}
-              onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
-              radius="md"
-            />
-          )}
+          <TextInput
+            label="Name"
+            placeholder="Your name"
+            value={form.values.name}
+            onChange={(event) =>
+              form.setFieldValue("name", event.currentTarget.value)
+            }
+            radius="md"
+          />
 
           <TextInput
             required
             label="Email"
             placeholder="hello@mantine.dev"
             value={form.values.email}
-            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+            onChange={(event) =>
+              form.setFieldValue("email", event.currentTarget.value)
+            }
             error={form.errors.email}
             radius="md"
           />
@@ -71,34 +79,30 @@ function RegistrationPage(props: PaperProps) {
             label="Password"
             placeholder="Your password"
             value={form.values.password}
-            onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+            onChange={(event) =>
+              form.setFieldValue("password", event.currentTarget.value)
+            }
             error={form.errors.password}
             radius="md"
           />
 
-          {type === 'register' && (
-            <Checkbox
-              label="I accept terms and conditions"
-              checked={form.values.terms}
-              onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
-            />
-          )}
+          <Checkbox
+            label="I accept terms and conditions"
+            checked={form.values.terms}
+            onChange={(event) =>
+              form.setFieldValue("terms", event.currentTarget.checked)
+            }
+          />
         </Stack>
 
         <Group position="apart" mt="xl">
-          <Anchor
-            component="button"
-            type="button"
-            color="dimmed"
-            onClick={() => toggle()}
-            size="xs"
-          >
-            {type === 'register'
-              ? 'Already have an account? Login'
-              : "Don't have an account? Register"}
-          </Anchor>
+          <Link to="/login">
+            <Anchor component="button" type="button" color="dimmed" size="xs">
+              Already have an account? Login
+            </Anchor>
+          </Link>
           <Button type="submit" radius="xl">
-            {upperFirst(type)}
+            Register
           </Button>
         </Group>
       </form>
